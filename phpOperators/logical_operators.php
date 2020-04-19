a7fd85a43643bd8ed6027e64b4c4a5d362f8d6bf
Me <alicealmeyda72@gmail.com>
2020-04-19 17:23:19 -0300

    Operadores lógicos.

---
+++ phpOperators/logical_operators.php
@@ -0,0 +1,58 @@
+<?php
+
+//And
+$x = 100;
+$y = 50;
+
+if ($x == 100 and $y == 50){
+    echo "Hello World";
+}
+
+echo "<br>";
+
+//Or
+$x = 100;
+$y = 50;
+
+if ($x == 100 or $y == 80){
+    echo "Hello World";
+}
+
+echo "<br>";
+
+//XOR
+$x = 100;
+$y = 50;
+
+if ($x == 100 xor $y == 80){
+    echo "Hello World";
+}
+
+echo "<br>";
+
+//And
+$x = 100;
+$y = 50;
+
+if ($x == 100 && $y == 50){
+    echo "Hello World";
+}
+
+echo "<br>";
+
+//Or
+$x = 100;
+$y = 50;
+
+if ($x == 100 || $y == 80){
+    echo "Hello World";
+}
+
+echo "<br>";
+
+//Not
+$x = 100;
+
+if ($x != 90){
+    echo "Hello World";
+}
