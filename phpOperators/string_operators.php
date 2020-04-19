aff7f36a8c97b84cda4835ec43e7088db05b96e4
Me <alicealmeyda72@gmail.com>
2020-04-19 17:28:19 -0300

    Operador de string.

---
+++ phpOperators/string_operators.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<br>";
+
+//Concatenation
+
+$txt1 = "Hello";
+$txt2 = "World";
+
+echo $txt1.$txt2;
+
+echo "<br>";
+
+//Concatenation assignment	
+
+$txt1 = "Hello";
+$txt2 = "World";
+$txt1 .= $txt2;
+
+echo $txt1;
