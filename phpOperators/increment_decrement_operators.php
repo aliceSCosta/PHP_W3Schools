dcf51d727c039027ffdc41c0222533cbde1b57ae
Me <alicealmeyda72@gmail.com>
2020-04-19 17:08:51 -0300

    Operadores de incrementar e decrementar.

---
+++ phpOperators/increment_decrement_operators.php
@@ -0,0 +1,23 @@
+<?php
+
+# Pre-increment	
+$x = 10;
+echo ++$x; //Incrementa +1 e retorna $x já incrementado
+
+echo "<br>";
+
+# Post-increment	
+$x = 10;
+echo $x++; //Retorna $x e depois que incrementa +1
+
+echo "<br>";
+
+# Pre-decrement	
+$x = 10;  
+echo --$x; //Decrementa -1 e retorna $x
+
+echo "<br>";
+
+# Post-decrement	
+$x = 10;
+echo $x--; //Retorna o valor de $x e depois decrementa
