9aa13e890736484ab6c7322f1e5b21cb0d33905a
Me <alicealmeyda72@gmail.com>
2020-04-19 14:36:53 -0300

    Criando uma constante.

---
+++ phpConstants/constant.php
@@ -0,0 +1,11 @@
+<?php
+
+# criando uma constante define("nome", valor, case-insensitive) 
+
+# caso sensitivo já vem definido como falso por padrão
+
+define("JOIN", 35235);
+echo JOIN;
+
+define("TeSt", "Again", true);
+echo test;
