87c6458f5e088632aaa0697fe4ab3844e047e587
Me <alicealmeyda72@gmail.com>
2020-04-19 13:38:46 -0300

    Verificando se o valor de uma variável é inteiro.

---
+++ phpNumbers/integers.php
@@ -0,0 +1,12 @@
+<?php
+
+# "is_int()" função que testa se o valor de uma variável é inteiro
+# is_integer() - alias of is_int()
+# is_long() - alias of is_int()
+
+$x = 32613;
+var_dump(is_int($x));
+
+
+$x = 26.105;
+var_dump(is_int($x));
