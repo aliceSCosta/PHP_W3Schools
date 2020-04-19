3fe78580a78c31870ecd3331d6d9189846508b27
Me <alicealmeyda72@gmail.com>
2020-04-19 16:22:47 -0300

    Operadores de atribuição.

---
+++ phpOperators/assignment_operators.php
@@ -0,0 +1,42 @@
+<?php
+
+echo "<br>";
+
+// Atribui o valor da direita à variável da esquerda
+$x = 10;
+echo $x;
+
+echo "<br>";
+
+//Addition
+$x = 10;
+$x += 100;
+echo $x;
+
+echo "<br>";
+
+//Subtraction
+$x = 20;
+$x -= 10;
+echo $x;
+
+echo "<br>";
+
+//Multiplication
+$x = 10;
+$x *= 2;
+echo $x;
+
+echo "<br>";
+
+//Division
+$x = 20;
+$x /= 2;
+echo $x;
+
+echo "<br>";
+
+//Modulus
+$x = 30;
+$x %= 2;
+echo $x;
