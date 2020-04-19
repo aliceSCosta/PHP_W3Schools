a0a83b86993b38d7ba8da023ff4fb7e88d8138eb
Me <alicealmeyda72@gmail.com>
2020-04-19 13:50:45 -0300

    Tipo de dado para valores que ultrapassam float.

---
+++ phpNumbers/infinity.php
@@ -0,0 +1,14 @@
+<?php
+
+# infinity para valores maiores que o tamanho máximo do float
+
+# is_finite verifica se o valor da variável é finito
+# is_infinity verifica se o valor da variável é infinito
+
+$x = 235E26135;
+var_dump($x);
+
+$x = 56;
+var_dump($x);
+
+# "var_dump" retorna o tipo e o valor dos dados
