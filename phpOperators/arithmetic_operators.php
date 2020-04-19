4319301e75da1607779b2953b69f8bbc45a9823d
Me <alicealmeyda72@gmail.com>
2020-04-19 16:06:49 -0300

    Operadores Aritméticos .

---
+++ phpOperators/arithmetic_operators.php
@@ -0,0 +1,53 @@
+<?php
+
+// Addition
+$x = 10;
+$y = 15;
+
+$result = $x + $y;
+echo $result;
+
+echo "<br>";
+
+// Subtraction
+$x = 56;
+$y = 156;
+
+$result = $x - $y;
+echo $result;
+
+echo "<br>";
+
+// Multiplication
+$x = 15;
+$y = 15;
+
+$result = $x * $y;
+echo $result;
+
+echo "<br>";
+
+//Division
+$x = 156;
+$y = 15;
+
+$result = $x / $y;
+echo $result;
+
+echo "<br>";
+
+//Modulus
+$x = 156;
+$y = 10;
+
+$result = $x % $y;
+echo $result;
+
+echo "<br>";
+
+//Exponatiation
+$x = 12;
+$y = 5;
+
+$result = $x ** $y;
+echo $result;
