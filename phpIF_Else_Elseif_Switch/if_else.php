c47a2116e84d5b158cfc4a6040dd1cee5a7bb653
Me <alicealmeyda72@gmail.com>
2020-04-20 19:50:38 -0300

    Estrutura de condição com If e Else

---
+++ phpIF_Else_Elseif_Switch/if_else.php
@@ -0,0 +1,10 @@
+<?php
+
+$t = date("H");
+
+if($t < 20){
+    echo "Have a good day";     //executa esse código se o resultado da condição for verdadeira
+}
+else{
+    echo "Have a good night";   //executa esse bloco de código se o resultado da condição for falsa
+}
