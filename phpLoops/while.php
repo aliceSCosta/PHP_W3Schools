f9d7bb296057426bff03eaf31a55429af1f30928
Me <alicealmeyda72@gmail.com>
2020-04-20 20:29:03 -0300

    While repete um bloco de código enquanto a condição for verdadeira.

---
+++ phpLoops/while.php
@@ -0,0 +1,17 @@
+<?php
+
+//percorre um bloco de código desde que a condição especificada seja verdadeira
+
+$x = 1;
+
+while ($x < 5){
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+$x = 0;
+
+while ($x <= 100){
+    echo "The number is: $x <br>";
+    $x+=10;
+}
