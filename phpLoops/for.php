8996a40f4fade73e1ecf0b1f36c03dd29c601e91
Me <alicealmeyda72@gmail.com>
2020-04-20 20:40:43 -0300

    Laço for usado quando se sabe antecipadamente quantas vezes quer que um código seja executado.

---
+++ phpLoops/for.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "<br>";
+
+/*O loop for é usado quando você sabe com antecedência quantas vezes o script deve ser executado.*/
+
+#Parâmetros
+
+//Contador de inicialização
+//Teste do Contador
+//Incrementação do contador
+
+for($x=0; $x <= 10; $x++){
+    echo "The number is: $x <br>";
+}
+
+echo "<br>";
+
+for($x=0; $x <= 100; $x+=10){
+    echo "The number is: $x <br>";
+}
