6e112f0f55be3dc1dda19bab78e48a42269a2949
Me <alicealmeyda72@gmail.com>
2020-04-30 15:36:46 -0300

    Array associado com chave e valor.

---
+++ phpArrays/associative_arrays.php
@@ -0,0 +1,7 @@
+<?php
+
+//Matrizes associativas são matrizes que usam chaves nomeadas que você atribui a elas
+
+$age = array('Alice' => 18, 'Gustavo' => 14, 'Vitória' => 13);
+
+echo "Alice is " . $age["Alice"] . " years old";
